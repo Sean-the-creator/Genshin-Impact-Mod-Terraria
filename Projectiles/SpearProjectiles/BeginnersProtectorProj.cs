3bf021e84c872312592fd3f168a8bb68c018c440
Sean-the-creator <93686490+Sean-the-creator@users.noreply.github.com>
2021-12-08 19:50:51 -0700

    Add files via upload

---
+++ Projectiles/SpearProjectiles/BeginnersProtectorProj.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace GenshinImpactMod.Projectiles.SpearProjectiles
+{
+	class BeginnersProtectorProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Beginner's Protector");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 72;
+			projectile.height = 70;
+			projectile.aiStyle = 19;
+			projectile.penetrate = -1;
+			projectile.scale = 1.3f;
+			projectile.alpha = 0;
+			projectile.hide = true;
+			projectile.ownerHitCheck = true;
+			projectile.melee = true;
+			projectile.tileCollide = false;
+			projectile.friendly = true;
+		}
+
+		public float movementFactor
+		{
+			get => projectile.ai[0];
+			set => projectile.ai[0] = value;
+		}
+
+		public override void AI()
+		{
+			Player projOwner = Main.player[projectile.owner];
+			Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+			projectile.direction = projOwner.direction;
+			projOwner.heldProj = projectile.whoAmI;
+			projOwner.itemTime = projOwner.itemAnimation;
+			projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+			projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+
+			if (!projOwner.frozen)
+			{
+				if (movementFactor == 0f)
+				{
+					movementFactor = 3f;
+					projectile.netUpdate = true;
+				}
+				if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3)
+				{
+					movementFactor -= 2.4f;
+				}
+				else
+				{
+					movementFactor += 2.1f;
+				}
+			}
+
+			projectile.position += projectile.velocity * movementFactor;
+
+			if (projOwner.itemAnimation == 0)
+			{
+				projectile.Kill();
+			}
+
+			projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(135f);
+
+			if (projectile.spriteDirection == -1)
+			{
+				projectile.rotation -= MathHelper.ToRadians(90f);
+			}
+		}
+	}
+}
