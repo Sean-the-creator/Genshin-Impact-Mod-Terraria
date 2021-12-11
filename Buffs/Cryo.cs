6c632d6e56014d83e0ab524669dcb74215853873
Caleb-sbani <67887509+Caleb-sbani@users.noreply.github.com>
2021-12-10 22:43:18 -0500

    Cryo Debuff
    implements cryo debuff and adds a modplayer class, this will be very useful for combination of different debuffs.
    
    Update GenshinImpactMod.csproj.AssemblyReference.cache
    
    Create .gitignore

---
+++ Buffs/Cryo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using GenshinImpactMod;
+
+namespace GenshinImpactMod.Buffs
+{
+	//warning: the debuff is untested (cannot be tested without creating a method of applying it) - but should work : if you have problems talk to EatMySocks.jpg
+    class Cryo : ModBuff
+    {
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Cryo");
+			Description.SetDefault("Affected by cryo affects");
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = false;
+			canBeCleared = true;
+		}
+
+		//used for calculating when damage ticks should happen
+		int DmgCounter = 0;
+
+        public override void Update(Terraria.Player player, ref int buffIndex)
+        {
+			Player p = player.GetModPlayer<Player>();
+			//  ^ not necessary but may be used for combination of attacks!
+			
+			//this function is called every tick, so the player should get damaged by 6 damage a second
+			player.moveSpeed -= 0.05f;
+			if(DmgCounter == 1)
+            {
+				player.statLife -= 2;
+            }
+			DmgCounter++;
+			if(DmgCounter >= 20)
+            {
+				DmgCounter = 0;
+            }
+        }
+    }
+}
