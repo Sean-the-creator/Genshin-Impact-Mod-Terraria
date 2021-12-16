6607a0417febeb5367594b92aeaa8e50c173ee48
Sean-the-creator <sean@weidlich.ca>
2021-12-15 19:04:45 -0700

    test sussy

---
+++ GlobalProperties/GenshinGlobalNPC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using GenshinImpactMod.Items;
+
+namespace GenshinImpactMod.GlobalProperties
+{
+    class GenshinGlobalNPC : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            if (npc.type == NPCID.SkeletronHead && !Main.expertMode)
+            {
+                Item.NewItem(npc.getRect(), ModContent.ItemType<AcquiantFate>());
+            }
+        }
+    }
+}
