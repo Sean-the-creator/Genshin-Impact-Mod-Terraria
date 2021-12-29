659f4a7a6914c675e3657992223f2adaab0d5826
Sean-the-creator <sean@weidlich.ca>
2021-12-28 21:40:19 -0700

    changed

--- Projectiles/ArmourSummons/Guoba.cs
+++ Projectiles/ArmourSummons/Guoba.cs
@@ -10,7 +10,11 @@ namespace GenshinImpactMod.Projectiles.ArmourSummons
     class Guoba: ModProjectile
     {
         #region Variables
+        private int timer = 0;
+        private int cycle = 0;
 
+        private bool Stateidle = true;
+        private bool Statefire = false;
         #endregion
 
         #region SetDefaults 
@@ -30,7 +34,33 @@ namespace GenshinImpactMod.Projectiles.ArmourSummons
         #endregion
         public override void AI()
         {
+            if (Stateidle)
+            {
+                timer++;
+                if (timer > 20)
+                {
+                    Stateidle = false;
+                    Statefire = true;
+                    timer = 0;
+                }
+            }
+            else if (Statefire)
+            {
+                timer++;
+                Projectile.NewProjectile(projectile.position.X, projectile.position.Y, 0, 0, ProjectileID.Flames, 25, 0);
+                if (timer > 20)
+                {
+                    Stateidle = true;
+                    Statefire = false;
+                    cycle++;
+                }
+            }
 
+
+            if (cycle > 4)
+            {
+                projectile.Kill();
+            }
         }
 
     }
