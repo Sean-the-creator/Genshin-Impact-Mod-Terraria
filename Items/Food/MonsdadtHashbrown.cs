d123f840f70012303d2cc102bbe185a494af3726
Sean-the-creator <sean@weidlich.ca>
2022-01-10 17:46:56 -0700

    r

---
+++ Items/Food/MonsdadtHashbrown.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using GenshinImpactMod;
+
+namespace GenshinImpactMod.Items.Food
+{
+    class MonsdadtHashbrown : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Monsdadt Hashbrown");
+            Tooltip.SetDefault("ARE THESE NUGGIES\n- Timmie");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = ;
+            item.height =;
+            item.useTurn = true;
+            item.maxStack = 30;
+            item.rare = ItemRarityID.Expert;
+            item.useAnimation = 15;
+            item.useTime = 15;
+            item.useStyle = ItemUseStyleID.EatingUsing;
+
+            item.UseSound = SoundID.Item2;
+
+            ModdedPlayer player = new ModdedPlayer();
+            if (player.CanEat)
+            {
+                item.stack--;
+                item.buffType = BuffID.WellFed;
+                item.buffTime = 10000;
+            }
+
+            item.value = 1;
+        }
+
+
+    }
+}
