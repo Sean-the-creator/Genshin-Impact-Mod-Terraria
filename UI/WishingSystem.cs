17e74efcee7c2178b3210d470f16c6a15b4b3a47
Sean-the-creator <sean@weidlich.ca>
2022-03-11 17:42:41 -0700

    e

--- UI/WishingSystem.cs
+++ UI/WishingSystem.cs
@@ -42,24 +42,32 @@ namespace GenshinImpactMod.UI
             WishSystemPanel = new DragableUI();
             WishSystemPanel.Left.Set(400f, 0f);
             WishSystemPanel.Top.Set(100f, 0f);
-            WishSystemPanel.Width.Set(600f, 0f);
-            WishSystemPanel.Height.Set(300f, 0f);
-            Texture2D wishButtonTexture = ModContent.GetTexture("Terraria/UI/ButtonPlay");
+            WishSystemPanel.Width.Set(500f, 0f);
+            WishSystemPanel.Height.Set(250f, 0f);
 
+            Texture2D wishButtonTexture = ModContent.GetTexture("Terraria/UI/ButtonPlay");
             UIHoverImageButton wishButton = new UIHoverImageButton(wishButtonTexture, "Wish");
-            wishButton.Left.Set(432f, 0f);
-            wishButton.Top.Set(64f, 0f);
-            wishButton.Width.Set(88f, 0f);
-            wishButton.Height.Set(88f, 0f);
+            wishButton.Left.Set(218f, 0f);
+            wishButton.Top.Set(192f, 0f);
+            wishButton.Width.Set(117f, 0f);
+            wishButton.Height.Set(45f, 0f);
             wishButton.OnClick += new MouseEvent(WishButtonClicked);
+
+            Texture2D wishButtonx10Texture = ModContent.GetTexture("Terraria/UI/ButtonPlay");
+            UIHoverImageButton wishButtonx10 = new UIHoverImageButton(wishButtonx10Texture, "Wish x 10");
+            wishButtonx10.Left.Set(348f, 0f);
+            wishButtonx10.Top.Set(192f, 0f);
+            wishButtonx10.Width.Set(117f, 0f);
+            wishButtonx10.Height.Set(45f, 0f);
+            wishButtonx10.OnClick += new MouseEvent(WishButtonx10Clicked);
 
 
             Texture2D deleteButtonTexture = ModContent.GetTexture("Terraria/UI/ButtonDelete");
             UIHoverImageButton closeButton = new UIHoverImageButton(deleteButtonTexture, Language.GetTextValue("LegacyInterface.52"));
-            closeButton.Left.Set(536f, 0f);
-            closeButton.Top.Set(240f, 0f);
-            closeButton.Width.Set(32f, 0f);
-            closeButton.Height.Set(32f, 0f);
+            closeButton.Left.Set(460f, 0f);
+            closeButton.Top.Set(8f, 0f);
+            closeButton.Width.Set(30f, 0f);
+            closeButton.Height.Set(30f, 0f);
             closeButton.OnClick += new MouseEvent(CloseButtonClicked);
 
 
@@ -67,11 +75,12 @@ namespace GenshinImpactMod.UI
             WishingSystemBanner = new UIImage(wishingSystemBanner);
             WishingSystemBanner.Left.Set(0f, 0f);
             WishingSystemBanner.Top.Set(0f, 0f);
-            WishingSystemBanner.Width.Set(600f, 0);
-            WishingSystemBanner.Height.Set(300f, 0f);
+            WishingSystemBanner.Width.Set(500f, 0);
+            WishingSystemBanner.Height.Set(250f, 0f);
 
             WishSystemPanel.Append(wishButton);
             WishSystemPanel.Append(closeButton);
+            WishSystemPanel.Append(wishButtonx10);
             WishSystemPanel.Append(WishingSystemBanner);
             Append(WishSystemPanel);
         }
@@ -107,6 +116,47 @@ namespace GenshinImpactMod.UI
                         Main.PlaySound(SoundID.Pixie);
                         for (int x = 0; x < 10; x++) { int dust = Dust.NewDust(player.position, player.height, player.width, DustID.Clentaminator_Cyan, random.Next(-3, 3), random.Next(-3, 3)); }
                         break;
+                    }
+                }
+
+            }
+        }
+
+        private void WishButtonx10Clicked(UIMouseEvent evt, UIElement listeningElement)
+        {
+            for (int s = 0; s < 255; s++)
+            {
+                Player player = Main.player[s];
+                if (!player.active)
+                {
+                    continue;
+                }
+
+                foreach (Item item in player.inventory)
+                {
+                    Random random = new Random();
+                    if (item.type == ModContent.ItemType<AcquiantFate>() && item.stack >= 10)
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            int wishItem = items[random.Next(0, items.Length)];
+                            if (wishItem == ModContent.ItemType<XianglingHelmet>())
+                            {
+                                player.QuickSpawnItem(ModContent.ItemType<XianglingHelmet>());
+                                player.QuickSpawnItem(ModContent.ItemType<XianglingBreastplate>());
+                                player.QuickSpawnItem(ModContent.ItemType<XianglingLeggings>());
+                            }
+                            else
+                            {
+                                player.QuickSpawnItem(wishItem);
+                            }
+                            item.stack--;
+                            Main.PlaySound(SoundID.Pixie);
+                            for (int x = 0; x < 10; x++) { int dust = Dust.NewDust(player.position, player.height, player.width, DustID.Clentaminator_Cyan, random.Next(-3, 3), random.Next(-3, 3)); }
+                            
+                        }
+                        break;
+                        
                     }
                 }
 
